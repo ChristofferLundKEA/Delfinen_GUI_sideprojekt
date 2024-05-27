360640834cdd6e04618f8a4b7d39c1e7b7898c34
ChristofferLundKEA <chlu0002@stud.kea.dk>
2024-05-27 19:23:05 +0200

    1.1

--- src/main/java/com/example/webeswimming/Main.java
+++ src/main/java/com/example/webeswimming/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         launch();
     }
 }
